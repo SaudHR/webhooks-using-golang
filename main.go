dff85b8b56a1023b32758a0d10dca60d6a28451e
SaudHR <98830103+SaudHR@users.noreply.github.com>
2022-07-24 21:21:56 +0300

    Update main.go

--- main.go
+++ main.go
@@ -8,10 +8,14 @@ import (
 
 func wenhook(w http.ResponseWriter, r *http.Request) {
 	log.Println("Received webhook...")
-	body := r.Body
+	body := w.Header()
+	log.Println("the webhook...: %s", body)
 	fmt.Fprintf(w, "The body is: %s", body)
 }
 
+// This was a test for github webhook
+// I get the printout but not the body
+// I don't know how to do it yet....
 func main() {
 	http.HandleFunc("/webhook", wenhook)
 	http.ListenAndServe(":8080", nil)
