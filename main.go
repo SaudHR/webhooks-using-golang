ef6ba79a7bd1c4136a22e2cb3cbd4fd8008f15f3
SaudHR <98830103+SaudHR@users.noreply.github.com>
2022-07-24 21:06:15 +0300

    Update main.go

--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func wenhook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received webhook...")
 	body := r.Body
 	fmt.Fprintf(w, "The body is: %s", body)
 }
 
 func main() {
-	http.HandleFunc("/", wenhook)
+	http.HandleFunc("/webhook", wenhook)
 	http.ListenAndServe(":8080", nil)
 }
